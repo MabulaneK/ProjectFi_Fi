9d9bd1ee93695d04a051e4ebd960f4d49387ad78
Mabulane Katlego <mabulane.k@outlook.com>
2022-01-17 16:53:26 +0200

    first commit1

---
+++ Auth/auth.js
@@ -0,0 +1,18 @@
+var load =document.querySelector('submit');
+
+
+load.addEventListener("click", function(e){
+    e.preventDefault();
+    //window.location.href="admin.html";
+    //window.open("https://www.w3schools.com");
+  Window.load(href="/Log/forgotpwd.html");
+    
+
+});
+
+
+
+
+
+
+
